66e0322153bbdf1d8b319ae4f5cfc57ed4c2ee7a
Francesco Melchiori <franzmelchiori@gmail.com>
2018-02-20 16:49:54 +0100

    update scraped_strings module

--- scraped_strings.py
+++ scraped_strings.py
@@ -24,16 +24,20 @@ import json
 
 class StringManager:
 
-    def __init__(self, scraped_string, customer_name='test', path_json=''):
+    def __init__(self, scraped_string, customer_name='test', path_json='',
+                 map_norm=True):
+        self.scraped_string = str(scraped_string)
+        self.aos_scrap = 'no_aos_scrap'
+        self.id_scrap = 'no_id_scrap'
+        self.aos_name = 'no_aos_name'
+        self.id_session = 'no_id_session'
         self.customer_name = str(customer_name)
         self.path_json = str(path_json)
-        self.scraped_string = str(scraped_string)
-        self.aos_scrap = ''
-        self.id_scrap = ''
         self.customer_settings = {}
         if customer_name == 'test':
             self.store_json_customer_settings()
         self.load_json_customer_settings(self.path_json)
+        self.map_norm = map_norm
         self.extract_aos_name()
         self.extract_id_session()
 
@@ -42,12 +46,14 @@ class StringManager:
             print(self.aos_scrap)
         else:
             print('no aos name')
+        return True
 
     def print_id_session(self):
         if self.id_scrap != '':
             print(self.id_scrap)
         else:
             print('no id session')
+        return True
 
     def store_json_customer_settings(self):
         customer_settings = {'aos_names': ['aos_1', 'aos_2'],
@@ -59,6 +65,7 @@ class StringManager:
                   fp=open('{0}_customer_settings.json'.format(
                       self.customer_name), 'w'),
                   indent=4)
+        return True
 
     def load_json_customer_settings(self, path_json=''):
         filename_json = '{0}_customer_settings.json'.format(self.customer_name)
@@ -87,9 +94,13 @@ class StringManager:
             ax_title_marks = self.customer_settings['ax_title_marks']
             aos_start = ax_title_marks['aos_start']
             aos_stop = ax_title_marks['aos_stop']
-            self.aos_scrap = self.extract_text_scrap(mark_start=aos_start,
-                                                     mark_stop=aos_stop)
-            return self.aos_scrap
+            text_scrap = self.extract_text_scrap(mark_start=aos_start,
+                                                 mark_stop=aos_stop)
+            if text_scrap:
+                self.aos_scrap = text_scrap
+                if self.map_norm:
+                    self.map_aos_scrap()
+                return True
         return False
 
     def extract_id_session(self):
@@ -97,24 +108,53 @@ class StringManager:
             ax_title_marks = self.customer_settings['ax_title_marks']
             id_start = ax_title_marks['id_start']
             id_stop = ax_title_marks['id_stop']
-            self.id_scrap = self.extract_text_scrap(mark_start=id_start,
-                                                    mark_stop=id_stop)
-            return self.id_scrap
+            text_scrap = self.extract_text_scrap(mark_start=id_start,
+                                                 mark_stop=id_stop)
+            if text_scrap:
+                self.id_scrap = text_scrap
+                if self.map_norm:
+                    self.norm_id_session()
+                return True
+        return False
+
+    def map_aos_scrap(self):
+        if 'aos_names' in self.customer_settings:
+            aos_names = self.customer_settings['aos_names']
+            if self.aos_scrap in aos_names:
+                self.aos_name = self.aos_scrap
+                return True
+            else:
+                pass
+        return False
+
+    def norm_id_session(self):
+        try:
+            norm_id_scrap = int(''.join(self.id_scrap.split()))
+        except ValueError:
+            return False
+        if norm_id_scrap in range(2**16):
+            self.id_session = norm_id_scrap
+            return True
         return False
 
 
-def get_aos_id(scraped_string, customer_name='test', path_json=''):
+def get_aos_id(scraped_string, customer_name='test', path_json='',
+               map_norm=True):
     sm = StringManager(scraped_string=scraped_string,
                        customer_name=customer_name,
-                       path_json=path_json)
+                       path_json=path_json,
+                       map_norm=map_norm)
+    if map_norm:
+        return sm.aos_name, sm.id_session
     return sm.aos_scrap, sm.id_scrap
 
 
 if __name__ == "__main__":
 
-    scrap_example_us = "Inc. [AOS_1: Session ID - 1] - [1 -"
-    scrap_example_it = "S.p.A. [AOS_1: ID sessione - 1] - [1 -"
-    scrap_example_de = "GmbH [AOS_1: Session ID - 39] - [1 -"
+    scrap_example_us = "Inc. [a0s_1: Session ID - 1 2] - [1 -"
+    scrap_example_it = "S.p.A. [a0s_1: ID sessione - 1 2] - [1 -"
+    scrap_example_de = "GmbH [a0s_1: Session ID - 1 2] - [1 -"
 
     print(get_aos_id(scraped_string=scrap_example_us,
-                     customer_name='test'))
+                     customer_name='test',
+                     map_norm=True))
